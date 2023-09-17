3f8d8b8da37a47a67351f14b3cc048cd70e20415
Uniaut <kunwoo0927@gmail.com>
2023-09-17 23:10:11 +0900

    mnist example

---
+++ misc/mnist.py
@@ -0,0 +1,101 @@
+import numpy as np
+
+import matplotlib.pyplot as plt
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torchvision
+import torchvision.transforms as transforms
+
+# Define a neural network
+class Net(nn.Module):
+    def __init__(self):
+        super(Net, self).__init__()
+        self.conv1 = nn.Conv2d(1, 6, 5)
+        self.pool = nn.MaxPool2d(2, 2)
+        self.conv2 = nn.Conv2d(6, 16, 5)
+        self.fc1 = nn.Linear(16 * 4 * 4, 120)
+        self.fc2 = nn.Linear(120, 84)
+        self.fc3 = nn.Linear(84, 10)
+
+    def forward(self, x):
+        x = self.pool(torch.relu(self.conv1(x)))
+        x = self.pool(torch.relu(self.conv2(x)))
+        x = x.view(-1, 16 * 4 * 4)
+        x = torch.relu(self.fc1(x))
+        x = torch.relu(self.fc2(x))
+        x = self.fc3(x)
+        return x
+
+# Load MNIST dataset
+transform = transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.5,), (0.5,))])
+trainset = torchvision.datasets.MNIST(root='./data', train=True, download=True, transform=transform)
+trainloader = torch.utils.data.DataLoader(trainset, batch_size=4, shuffle=True)
+
+# Initialize the neural network and optimizer
+net = Net()
+criterion = nn.CrossEntropyLoss()
+optimizer = optim.AdamW(net.parameters(), lr=0.001)
+
+# Training the network
+for epoch in range(5):  # loop over the dataset multiple times
+    running_loss = 0.0
+    for i, data in enumerate(trainloader, 0):
+        inputs, labels = data
+        optimizer.zero_grad()
+        outputs = net(inputs)
+        loss = criterion(outputs, labels)
+        loss.backward()
+        optimizer.step()
+        running_loss += loss.item()
+        if i % 2000 == 1999:  # print every 2000 mini-batches
+            print(f"[{epoch + 1}, {i + 1:5d}] loss: {running_loss / 2000:.3f}")
+            running_loss = 0.0
+
+print("Finished Training")
+
+# Save the trained model
+PATH = "./mnist_net.pth"
+torch.save(net.state_dict(), PATH)
+print("Saved trained model")
+
+# Load and test the trained model
+net = Net()
+net.load_state_dict(torch.load(PATH))
+net.eval()
+
+# Load the test dataset£
+testset = torchvision.datasets.MNIST(root='./data', train=False, download=True, transform=transform)
+testloader = torch.utils.data.DataLoader(testset, batch_size=4, shuffle=False)
+
+# Function to show some test images
+def imshow(img):
+    img = img / 2 + 0.5  # unnormalize
+    npimg = img.numpy()
+    plt.imshow(np.transpose(npimg, (1, 2, 0)))
+    plt.show()
+
+dataiter = iter(testloader)
+images, labels = next(dataiter)
+
+# Show some images
+imshow(torchvision.utils.make_grid(images))
+print("Ground Truth:", " ".join(f"{labels[j]}" for j in range(4)))
+
+# Make predictions
+outputs = net(images)
+_, predicted = torch.max(outputs, 1)
+print("Predicted: ", " ".join(f"{predicted[j]}" for j in range(4)))
+
+# Test the network on the entire test dataset
+correct = 0
+total = 0
+with torch.no_grad():
+    for data in testloader:
+        images, labels = data
+        outputs = net(images)
+        _, predicted = torch.max(outputs.data, 1)
+        total += labels.size(0)
+        correct += (predicted == labels).sum().item()
+
+print(f"Accuracy of the network on the 10000 test images: {100 * correct / total}%")
